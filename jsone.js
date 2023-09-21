1d708deea619c3f06c8ec78a911ec04d3041bec7
mahdi <mahdinaghizade1234@gmail.com>
2023-09-21 20:06:07 +0330

    first

--- jsone.js
+++ jsone.js
@@ -1,6 +1,40 @@
-const bil=document.getElementById("bilit")
-bil.addEventListener("mousedown",console.log("hi"))
+function show_hide() { 
+var click = document.getElementById("list-items"); 
+if(click.style.display ==="none") { 
+click.style.display ="block"; 
+} else { 
+click.style.display ="none"; 
+} 
+} 
 
-function mamad(){
-    document.innerHtml="<div>ssdfgsdfsfsefsefsfsefse</div>"
-}
+
+
+
+function show_hide2() { 
+    var click = document.getElementById("list-items2"); 
+    if(click.style.display ==="none") { 
+    click.style.display ="block"; 
+    } else { 
+    click.style.display ="none"; 
+    } 
+    } 
+
+    function show_hide3() { 
+        var click = document.getElementById("list-items3"); 
+        if(click.style.display ==="none") { 
+        click.style.display ="block"; 
+        } else { 
+        click.style.display ="none"; 
+        } 
+        } 
+    
+
+
+// function show_hide2() { 
+//      var click2 = document.getElementsByClassName("zirmenu"); 
+//      if(click2.style.display ==="none") { 
+//     click2.style.display ="block"; 
+//         } else { 
+//      click2.style.display ="none"; 
+//     } 
+//      } 
